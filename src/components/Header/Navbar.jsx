d6d1d0d7aaece475b6b8eceede471e4d231243fb
ibrahimaltr17 <ibrahimaltr17@gmail.com>
2026-03-17 21:46:36 +0600

    Build Navbar

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import logo from '../../assets/logo.png'
+import { FaGithub } from "react-icons/fa";
 
 const Navbar = () => {
     return (
-        <div className="navbar bg-base-100 shadow-sm">
+        <div className="navbar bg-base-100 shadow-sm max-w-360 mx-auto px-5">
             <div className="navbar-start">
                 <div className="dropdown">
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -11,36 +13,30 @@ const Navbar = () => {
                     <ul
                         tabIndex="-1"
                         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-                        <li><a>Item 1</a></li>
-                        <li>
-                            <a>Parent</a>
-                            <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </li>
-                        <li><a>Item 3</a></li>
+                        <li><a>Home</a></li>
+                        <li><a>App</a></li>
+                        <li><a>Installation</a></li>
                     </ul>
                 </div>
-                <a className="btn btn-ghost text-xl">daisyUI</a>
+                <a className="btn btn-ghost text-xl">
+                    <div className='h-10 w-10'>
+                        <img src={logo} alt="" />
+                    </div>
+                    <h2 className='gradient-text font-bold'>HERO.IO</h2>
+                </a>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
-                    <li><a>Item 1</a></li>
-                    <li>
-                        <details>
-                            <summary>Parent</summary>
-                            <ul className="p-2 bg-base-100 w-40 z-1">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </details>
-                    </li>
-                    <li><a>Item 3</a></li>
+                    <li><a>Home</a></li>
+                    <li><a>App</a></li>
+                    <li><a>Installation</a></li>
                 </ul>
             </div>
-            <div className="navbar-end">
-                <a className="btn">Button</a>
+            <div className="navbar-end ">
+                <a className="btn gradient-background text-white">
+                    <FaGithub />
+                    <h4>Contribute</h4>
+                </a>
             </div>
         </div>
     );
